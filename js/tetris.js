bd4d0b2721d77e4b7e08e2cd01d1aff326e99f85
Skylar Graika <skylar@10000ft.com>
2016-09-12 17:47:49 -0700

    updated new html5 version.

--- js/tetris.js
+++ js/tetris.js
@@ -13,22 +13,24 @@ function addRows() {
     for (let i = 0; i < NUMBER_ROWS; i++) {
         if (i >= NUMBER_ROWS - PEOPLE_ROWS) {
             if (firstPerson != true) {
-                $('#game-body').append('<div class="person-row first"></div>');
+                $('#game-body').append('<div class="row person first"><div class="person-zone"></div><div class="play-zone"></div></div>');
                 firstPerson = true;
             } else {
-                $('#game-body').append('<div class="person-row"></div>');
+                $('#game-body').append('<div class="row person"><div class="person-zone"></div><div class="play-zone"></div></div>');
             }
 
         } else {
-            $('#game-body').append('<div ></div>');
+            $('#game-body').append('<div class="row"><div class="dead-zone">|</div><div class="play-zone"></div></div>');
         }
 
     }
 }
 
 function addPeople() {
-    $('.person-row').html('');
-    $('.person-row').append('<div class="thumbnail-person"></div>')
+    $('.person-zone').html('');
+    $('.dead-zone').html('');
+    $('.person-zone').append('<div class="thumbnail-person"></div>')
+    $('.dead-zone').append('<div class="dotted-line"></div>')
 }
 
 function getRandomPeople() {
